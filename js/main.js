1865f99aa99e1a32848235c1a76ee2f1ab6fcfbd
Yuli Yang <yuli@figmints.com>
2013-11-11 16:07:20 -0500

    add content

--- js/main.js
+++ js/main.js
@@ -1,9 +1,39 @@
 $(document).ready(function() {
-	// returns height of browser viewport
-    // var topHeight = $(window).height();
-    // topHeight = topHeight - 90;
-    // $('#call-to-action').css('height', topHeight);
+  $('header').waypoint('sticky');
 
-    // var browserHeight = $(window).height();
-    // $('section').css('height', browserHeight);
+  $('#service .content i').hover(function() {
+      $(this).parent().find('p').toggleClass('active');
+      $(this).parent().find('span').toggleClass('active');
+  });
+
+  function hideAllProcess() {
+      $('#process-idea').hide();
+      $('#process-plan').hide();
+      $('#process-puff').hide();
+      $('#process-boil').hide();
+      $('#process-lab').hide();
+  }
+
+  hideAllProcess();
+  $('#process-idea').show();
+
+  $('.large-7.columns.ideapuffProcess i').on('click', function() {
+      $('.large-7.columns.ideapuffProcess i').removeClass('active');
+      $(this).addClass('active');
+      var processId = $(this).find('input').val();
+      hideAllProcess();
+      $('#' + processId).show();
+  });
+
+  $('a.right.processBtn').click(function(){
+      $(this).parents('.large-5.columns').hide();
+      $(this).parents('.large-5.columns').next('.large-5.columns').show();
+      $('.large-7.columns.ideapuffProcess i.active').removeClass('active').next('i').addClass('active');
+  });
+
+  $('a.left.processBtn').click(function(){
+      $(this).parents('.large-5.columns').hide();
+      $(this).parents('.large-5.columns').prev('.large-5.columns').show();
+      $('.large-7.columns.ideapuffProcess i.active').removeClass('active').prev('i').addClass('active');
+  });
 });
